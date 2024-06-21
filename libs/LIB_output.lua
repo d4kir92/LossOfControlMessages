51b7998c651ebd9d53feec1ff1e87c985f8db4f7
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-06-21 13:58:11 +0200

    v1.2.33

--- libs/LIB_output.lua
+++
@@ -1,6 +0,0 @@
--- LIB Output
-local _, LocMessages = ...
-
-function LocMessages:MSG(str)
-	print("|c0000ffff" .. "[" .. "|cff8888ff" .. "LossOfControlMessages" .. "|c0000ffff" .. "] " .. str)
-end
