3708d96873e0ae699f743173e497df85659bf054
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-07-10 12:23:38 +0200

    v1.2.35

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ local AddonName, LocMessages = ...
 local L = LibStub("AceLocale-3.0"):GetLocale("D4KIRLOCMessagesHelper")
 function LocMessages:InitSetting()
 	local LOCTAB_Settings = {}
-	LocMessages:SetVersion(AddonName, 135860, "1.2.34")
+	LocMessages:SetVersion(AddonName, 135860, "1.2.35")
 	LOCTAB_Settings.panel = CreateFrame("Frame", "LOCTAB_Settings", UIParent)
 	LOCTAB_Settings.panel.name = "LossOfControlMessages |T135860:16:16:0:0|t by |cff3FC7EBD4KiR |T132115:16:16:0:0|t"
 	local BR = 16
