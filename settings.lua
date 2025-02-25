5a10295b920ae30a419f44ae9e6929b6b18764f6
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-02-25 19:22:06 +0100

    v1.2.59

--- settings.lua
+++ settings.lua
@@ -14,15 +14,13 @@ end
 local BR = 16
 local LOCTypes = {"DISARM", "STUN_MECHANIC", "STUN", "PACIFYSILENCE", "SILENCE", "FEAR", "CHARM", "PACIFY", "CONFUSE", "POSSESS", "SCHOOL_INTERRUPT", "ROOT", "FEAR_MECHANIC", "NONE"}
 function LocMessages:InitSetting()
-	LOCTABPC = LOCTABPC or {}
-	LocMessages:SetVersion(AddonName, 135860, "1.2.58")
 	loc_settings = LocMessages:CreateFrame(
 		{
 			["name"] = "LOC Messages",
 			["pTab"] = {"CENTER"},
 			["sw"] = 520,
 			["sh"] = 520,
-			["title"] = format("LOC Messages |T135860:16:16:0:0|t v|cff3FC7EB%s", "1.2.58")
+			["title"] = format("|T135860:16:16:0:0|t LOC Messages v|cff3FC7EB%s", LocMessages:GetVersion())
 		}
 	)
 
@@ -181,12 +179,14 @@ frame:RegisterEvent("PLAYER_LOGIN")
 function frame:OnEvent(event, addonName, ...)
 	if event == "ADDON_LOADED" and addonName == AddonName then
 		frame:UnregisterEvent("ADDON_LOADED")
+		LOCTABPC = LOCTABPC or {}
+		LocMessages:SetVersion(135860, "1.2.59")
 		LocMessages:CreateMinimapButton(
 			{
 				["name"] = "LocMessages",
 				["icon"] = 135860,
 				["dbtab"] = LOCTABPC,
-				["vTT"] = {{"LocMessages |T135860:16:16:0:0|t", "v|cff3FC7EB1.2.58"}, {"Leftclick", "Toggle Settings"}, {"Rightclick", "Hide Minimap Icon"}},
+				["vTT"] = {{"|T135860:16:16:0:0|t LocMessages", "v|cff3FC7EB" .. LocMessages:GetVersion()}, {LocMessages:Trans("LID_LEFTCLICK"), LocMessages:Trans("LID_OPENSETTINGS")}, {LocMessages:Trans("LID_RIGHTCLICK"), LocMessages:Trans("LID_HIDEMINIMAPBUTTON")}},
 				["funcL"] = function()
 					LocMessages:ToggleSettings()
 				end,
