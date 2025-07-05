ce4bc26cf118ab8b3bae52ee44bfd7fec84c161e
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-07-05 09:02:50 +0200

    v1.2.72

--- core.lua
+++ core.lua
@@ -195,7 +195,7 @@ f_loc:SetScript(
 						if LocMessages:GetConfig("showinenglishonly", false) then
 							LocMessages:ToCurrentChat(string.format(locEng, text, LocMessages:MathR(duration, 1)))
 						else
-							LocMessages:ToCurrentChat(string.format(LocMessages:Trans("loctext"), text, LocMessages:MathR(duration, 1)))
+							LocMessages:ToCurrentChat(string.format(LocMessages:Trans("LID_loctext"), text, LocMessages:MathR(duration, 1)))
 						end
 					end
 
