f1cd3874e76be09cf673f9752527178da7e9c4ba
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-01-29 15:30:37 +0100

    v1.2.86

--- settings.lua
+++ settings.lua
@@ -161,7 +161,7 @@ function frame:OnEvent(event, addonName, ...)
 	if event == "ADDON_LOADED" and addonName == AddonName then
 		frame:UnregisterEvent("ADDON_LOADED")
 		LOCTABPC = LOCTABPC or {}
-		LocMessages:SetVersion(135860, "1.2.85")
+		LocMessages:SetVersion(135860, "1.2.86")
 		LocMessages:CreateMinimapButton(
 			{
 				["name"] = "LocMessages",
